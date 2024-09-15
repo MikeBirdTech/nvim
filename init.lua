1fce4355ff663a55569335761a53bc75d8eff7eb
Mike Bird <mike@arrendy.com>
2024-09-14 22:56:42 -0400

    Added nvim-tree.lua plugin with custom configuration and keybindings for file explorer toggle and find current file.

--- init.lua
+++ init.lua
@@ -145,7 +145,6 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
@@ -879,13 +878,59 @@ require("lazy").setup({
 			-- },
 		},
 	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					width = {
+						min = 30,
+						max = 50,
+					},
+					relativenumber = true,
+				},
+				renderer = {
+					group_empty = true,
+					highlight_git = true,
+					icons = {
+						show = {
+							git = true,
+						},
+					},
+				},
+				filters = {
+					dotfiles = false,
+				},
+				git = {
+					ignore = false,
+				},
+				actions = {
+					open_file = {
+						quit_on_open = false,
+					},
+				},
+			})
 
-	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-	-- init.lua. If you want these files, they are in the repository, so you can just download them and
-	-- place them in the correct locations.
+			-- Custom highlights
+			vim.cmd([[highlight NvimTreeGitIgnored guifg=#808080 ctermfg=grey]])
 
-	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-	--
+			-- Keymaps
+			vim.keymap.set(
+				"n",
+				"<leader>e",
+				":NvimTreeToggle<CR>",
+				{ noremap = true, silent = true, desc = "Toggle file explorer" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>nf",
+				":NvimTreeFindFile<CR>",
+				{ noremap = true, silent = true, desc = "Find current file in explorer" }
+			)
+		end,
+	},
+
 	--  Here are some example plugins that I've included in the Kickstart repository.
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
@@ -893,7 +938,6 @@ require("lazy").setup({
 	-- require 'kickstart.plugins.indent_line',
 	-- require 'kickstart.plugins.lint',
 	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
 	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
