d1b4541e3de67835061db14f80312168efcc756c
Mike Bird <mike@arrendy.com>
2024-09-15 01:21:53 -0400

    Added "leap.nvim" plugin with custom keybindings and highlighting configuration.

--- init.lua
+++ init.lua
@@ -897,7 +897,34 @@ require("lazy").setup({
 			)
 		end,
 	},
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			local leap = require("leap")
 
+			-- Set up bidirectional search with 's'
+			vim.keymap.set({ "n", "x", "o" }, "s", function()
+				leap.leap({ target_windows = { vim.fn.win_getid() } })
+			end)
+
+			-- Keep 'S' for searching in all windows
+			vim.keymap.set({ "n", "x", "o" }, "S", function()
+				leap.leap({
+					target_windows = vim.tbl_filter(function(win)
+						return vim.api.nvim_win_get_config(win).focusable
+					end, vim.api.nvim_tabpage_list_wins(0)),
+				})
+			end)
+
+			-- Optional: make highlights subtle
+			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+			vim.api.nvim_set_hl(0, "LeapMatch", {
+				fg = "grey",
+				bold = true,
+				nocombine = true,
+			})
+		end,
+	},
 	--  Here are some example plugins that I've included in the Kickstart repository.
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
