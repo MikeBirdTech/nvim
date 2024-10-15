7e187fcddeea11e5aca23455aa09dec22ecf107a
Mike Bird <63524998+MikeBirdTech@users.noreply.github.com>
2024-10-14 20:02:24 -0400

    Improve Telescope plugin configuration with follow and hidden defaults, and update find files keymap to follow symbol links.

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -22,6 +22,8 @@ return {
 			telescope.setup({
 				defaults = {
 					prompt_prefix = "🔍 ",
+					follow = true,
+					hidden = true,
 				},
 				extensions = {
 					["ui-select"] = {
@@ -36,7 +38,9 @@ return {
 			-- Keymaps
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sf", function()
+				builtin.find_files({ follow = true })
+			end, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
